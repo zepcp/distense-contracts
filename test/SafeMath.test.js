f6fab63b055d174b623dc2039a3b3815994c7c7e
JohnAllen <johnjalleniii@gmail.com>
2018-01-10 17:13:21 -0800

    Add back SafeMath percent test

--- test/SafeMath.test.js
+++ test/SafeMath.test.js
@@ -4,11 +4,11 @@ const SafeMathMock = artifacts.require('SafeMathMock.sol')
 
 
 contract('SafeMath', function () {
-  let safeMath;
+  let safeMath
 
   before(async function () {
     safeMath = await SafeMathMock.new();
-  });
+  })
 
   it('multiplies correctly', async function () {
     let a = 5678;
@@ -17,7 +17,6 @@ contract('SafeMath', function () {
     let result = await safeMath.result();
     assert.equal(result, a * b);
   });
-
   it('adds correctly', async function () {
     let a = 5678;
     let b = 1234;
@@ -25,7 +24,7 @@ contract('SafeMath', function () {
     let result = await safeMath.result();
 
     assert.equal(result, a + b);
-  });
+  })
 
   it('subtracts correctly', async function () {
     let a = 5678;
@@ -34,7 +33,7 @@ contract('SafeMath', function () {
     let result = await safeMath.result();
 
     assert.equal(result, a - b);
-  });
+  })
 
   it('should throw an error if subtraction result would be negative', async function () {
     let a = 1234;
@@ -45,7 +44,7 @@ contract('SafeMath', function () {
     } catch (error) {
       assertJump(error);
     }
-  });
+  })
 
   it('should throw an error on addition overflow', async function () {
     let a = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
@@ -57,5 +56,45 @@ contract('SafeMath', function () {
     let a = 115792089237316195423570985008687907853269984665640564039457584007913129639933;
     let b = 2;
     await assertRevert(safeMath.multiply(a, b));
-  });
+  })
+
+  it('calculates percentages correctly',  async () => {
+
+    let num = 10
+    let denom = 100
+    let percent = await safeMath.percent(num, denom, 2)
+    let result = await safeMath.result();
+    assert.equal(result, 10)
+
+    num = 10
+    denom = 100
+    await safeMath.percent(num, denom, 4)
+    result = await safeMath.result();
+    assert.equal(result, 1000)
+
+    num = 16
+    denom = 100
+    await safeMath.percent(num, denom, 4)
+    result = await safeMath.result();
+    assert.equal(result, 1600)
+
+    num = 49
+    denom = 100
+    await safeMath.percent(num, denom, 0)
+    result = await safeMath.result();
+    assert.equal(result, 0)
+
+    num = 1000
+    denom = 100
+    await safeMath.percent(num, denom, 4)
+    result = await safeMath.result();
+    assert.equal(result, 100000)
+
+    num = 100
+    denom = 100
+    await safeMath.percent(num, denom, 2)
+    result = await safeMath.result();
+    assert.equal(result, 100)
+
+  })
 });

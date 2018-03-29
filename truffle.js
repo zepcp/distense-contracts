d78da2176c52961221f1f7b1148ef9d89ab353ed
JohnAllen <johnjalleniii@gmail.com>
2018-03-29 09:51:46 -0700

    Remove old configs from truffle.js

--- truffle.js
+++ truffle.js
@@ -21,13 +21,6 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01
     },
-    rinkebyLocal: {
-      host: 'localhost',
-      network_id: '4',
-      port: 4000,
-      gas: 6719227,
-      gasPrice: 50000000000
-    },
     ropstenFast: {
       host: '159.89.135.56',
       network_id: '3',
@@ -35,14 +28,6 @@ module.exports = {
       from: '0x80e774718b3781d4527f0ecb0751074778870800',
       gas: 4605201,
       gasPrice: 100000000000
-    },
-    ropstenRemote: {
-      host: '138.68.51.255',
-      network_id: '3',
-      port: 5000,
-      from: '0x0De5Be0E82493AcCb8E83d2aF99E72458249bBF9',
-      gas: 4605201,
-      gasPrice: 10000000000000
     }
   },
   solc: {

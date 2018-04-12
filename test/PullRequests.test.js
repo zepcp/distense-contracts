fb7c313f5e077db66e6e21c747fd8ecd08d9341c
JohnAllen <johnjalleniii@gmail.com>
2018-04-12 09:08:35 -0700

    Update issueDID test; remove faucet from migrations script

--- test/PullRequests.test.js
+++ test/PullRequests.test.js
@@ -386,7 +386,7 @@ contract('PullRequests', function(accounts) {
     assert.equal(eventArgs.taskId, pullRequest.taskId)
   })
 
-  it.only("should increment a contributor's Contributions DID correctly after a pull request reaches the required approvals", async function() {
+  it("should increment a contributor's Contributions DID correctly after a pull request reaches the required approvals", async function() {
     await didToken.issueDID(accounts[0], 1000000)
     await didToken.incrementDIDFromContributions(accounts[0], 1000000)
 
